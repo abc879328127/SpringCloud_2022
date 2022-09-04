d160cbe92d272523e5f9479aef07ec09006678c0
guojd <guojd@3clear.com>
2022-09-04 22:59:42 +0800

    eureka的自我保护机制，与discover使用

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -3,13 +3,15 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.advice.SysLog;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
-import com.atguigu.springcloud.utils.CustomExceptionEnum;
 import com.atguigu.springcloud.utils.ReturnResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
 * @author: guojd
@@ -25,6 +27,9 @@ public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @SysLog
     @PostMapping(value = "/payment/create")
     public ReturnResult create(@RequestBody Payment payment){
@@ -37,6 +42,19 @@ public class PaymentController {
     public ReturnResult create(@PathVariable("id") Long id){
         return ReturnResult.succeedReturn(paymentService.getPaymentById(id),serverPort);
 
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public ReturnResult discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("****element:"+ service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return ReturnResult.succeedReturn(this.discoveryClient);
     }
 
 }
