d415fb7a982ed6be476d5ba4f03fb422aba8b091
guojd <guojd@3clear.com>
2022-09-06 00:00:56 +0800

    整合consul，注册中心

--- cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/OrderConsulMain80.java
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/OrderConsulMain80.java
@@ -3,11 +3,20 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient //该注解用于想consul或者zookeeper作为注册中心时注册服务
 @SpringBootApplication
-public class PaymentMain8004 {
+public class OrderConsulMain80 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8004.class, args);
+        SpringApplication.run(OrderConsulMain80.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
     }
 }
--- cloud-providerconsul-payment8005/src/main/java/com/atguigu/springcloud/PaymentMain8005.java
+++ cloud-providerconsul-payment8005/src/main/java/com/atguigu/springcloud/PaymentMain8005.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient //该注解用于想consul或者zookeeper作为注册中心时注册服务
 @SpringBootApplication
-public class PaymentMain8004 {
+public class PaymentMain8005 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8004.class, args);
+        SpringApplication.run(PaymentMain8005.class, args);
     }
 }
